e18862918f719f7747155efe23ebd095ffe6cb40
woochulpark <dfight63@gmail.com>
2022-08-24 13:48:58 +0900

    button event

--- study_react/src/App.js
+++ study_react/src/App.js
@@ -19,12 +19,17 @@ class App extends Component{
     ]
   } 
 
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+  }
+
   render(){
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
+        {/*button event 지정(camelCase)*/}
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >Nice to meet you.</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
